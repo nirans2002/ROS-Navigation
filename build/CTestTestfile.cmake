199e0634c8a42f46e934ec44c6dcef0b6919c8bb
Niran S Narayanan <nirans2002@gmail.com>
2023-03-16 19:12:37 +0530

    urdf

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/niran/ros_projects/ROS-Navigation/src
+# Build directory: /home/niran/ros_projects/ROS-Navigation/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("explorer_bot")
